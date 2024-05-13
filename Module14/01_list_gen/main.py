07c66ba8d383274aa084bb5990254ccac4a85ed4
Борис Калинин <109675571+boris240873@users.noreply.github.com>
2024-05-13 21:01:46 +0300

    Add files via upload

---
+++ Module14/01_list_gen/main.py
@@ -0,0 +1,8 @@
+number_list = []
+number = int(input('Введите целое число: '))
+for i in range(1, number + 1):
+ if i % 2 != 0:
+  number_list.append(i)
+
+print('Список из нечётных чисел от одного до N: ', number_list)
+
